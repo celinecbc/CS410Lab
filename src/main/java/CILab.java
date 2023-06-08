3b7fc2e8b62801d4ed7182094651026c88502096
Celine Chiou <chiouceline@gmail.com>
2023-06-08 15:26:11 -0700

    Implemented CILab and started CILabTest. Added Junit Dependency.

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -13,8 +13,21 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
-        return false;
-    }
+        if (myString == null) {
+            return false;
+        }
 
+        // count number of capital letters in string & add to char array
+        int count = 0;
+        for (int i = 0; i < myString.length(); i++) {
+            char[] c = myString.toCharArray();
+            if (Character.isUpperCase(myString.charAt(i))) {
+                count++;
+            }
+        }
+
+        // count = 0 --> all lower ; count = myString.length() --> all upper
+        return (count == 0 || count == myString.length() || count == 1 && Character.isUpperCase(myString.charAt(0)));
+    }
 }
 
